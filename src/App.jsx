5b7c135a86e7d3722bb2d303ec9b114b64f3d8b9
Rudransh Bhardwaj <249.rudranshbhardwaj@gmail.com>
2025-05-20 23:49:08 +0530

    added percentage and animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [percentage, setPercentage] = useState(10);
+  const [percentage, setPercentage] = useState(0);
   const handleClick = () => {
     if (percentage < 400) {
       setPercentage((prev) => prev + 10);
@@ -16,11 +16,17 @@ function App() {
       <h1 className="text-amber-400">Rudransh Bhardwaj</h1>
       <div className={`h-2 border-amber-400 m-1 border w-[400px]`}>
         <div
-          className="bg-amber-200 h-full"
+          className="bg-amber-200 h-full transition-all duration-300"
           style={{ width: `${percentage}px` }}
         ></div>
       </div>
-      <button onClick={handleClick}>Increase</button>
+      <p className="text-sm text-gray-200 mt-1">{Math.ceil(percentage / 4)}%</p>
+      <button
+        onClick={handleClick}
+        className="mt-2 px-4 py-1 bg-amber-400 text-white rounded hover:bg-amber-500"
+      >
+        Increase
+      </button>
     </>
   );
 }
